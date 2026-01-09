dc1e314efea9026898353931aa7e8d965b6cb1af
Chrisli2712 <christoph-schaffrath@t-online.de>
2026-01-09 09:17:21 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, Query
 from backend.signals import generate_signals
+import json
 
 app = FastAPI(title="SpeedyCoinZales API")
 
+with open("holdings.json", "r") as f:
+    holdings = json.load(f)
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
-
 @app.get("/signal")
 def get_signals(lang: str = Query("de")):
-    return generate_signals(lang=lang)
+    return generate_signals(holdings, lang)
